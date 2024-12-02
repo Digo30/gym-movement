2f697b76e4ad3fc9dd34ec81a78721a9866ce21b
alvaro bernucci <alvarosenabernucci@hotmail.com>
2024-12-02 18:20:36 -0300

    Add show

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,22 +1,40 @@
 class GymsController < ApplicationController
 
+  before_action :set_gym, only: [:show]
+  before_action :set_date_hour, only: [:index, :show]
+
   def index
     @gyms = Gym.all
-    time_now = Time.current
-    @time_now = Time.current
-    one_hour_ago = 1.hour.ago
 
     @fluxos = @gyms.map do |gym|
-      [gym.id, gym.appointments.where("checkin_date = ? AND checkin_hour BETWEEN ? AND ?", Date.today, one_hour_ago, time_now).count]
+      [gym.id, gym.appointments.where("checkin_date = ? AND checkin_hour BETWEEN ? AND ?", Date.today, @one_hour_ago, @time_now).count]
     end.to_h
 
     @gender = @gyms.map do |gym|
       gender_count = gym.appointments.joins(:user)
-                      .where("checkin_date = ? AND checkin_hour BETWEEN ? AND ?", Date.today, one_hour_ago, time_now)
+                      .where("checkin_date = ? AND checkin_hour BETWEEN ? AND ?", Date.today, @one_hour_ago, @time_now)
                       .group("users.gender")
                       .count("users.gender")
       gender_count.default = 0
       [gym.id, gender_count]
     end.to_h
   end
+
+  def show
+    @images = @gym.appointments.joins(:user)
+        .where("checkin_date = ? AND checkin_hour BETWEEN ? AND ?", Date.today, @one_hour_ago, @time_now)
+        .pluck('users.user_image') .sample(3)
+  end
+
+  private
+
+  def set_gym
+    @gym = Gym.find(params[:id])
+  end
+
+  def set_date_hour
+    @time_now = Time.current
+    @one_hour_ago = 1.hour.ago
+  end
+
 end
