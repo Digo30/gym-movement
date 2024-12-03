4bb8946df859a73f830a48603e304f8e72c6f696
Rodrigo Silva <rocsdesigner@gmail.com>
2024-12-03 15:47:56 -0300

    my account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :profiles, dependent: :destroy
   has_one_attached :image
+  has_one_attached :profile_picture
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
