46937b05055b0ae298e7b6e7f700b689ae5f49bf
Vinicius Neumann <v.ogell@web.de>
2024-12-03 11:08:40 -0300

    geolocalizacao

---
+++ app/controllers/gyms_controller.rb
@@ -0,0 +1,12 @@
+class GymsController < ApplicationController
+  def index
+    @flats = Flat.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
+  end
+end
--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,3 +1,5 @@
 class Gym < ApplicationRecord
   has_many :appointments, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
