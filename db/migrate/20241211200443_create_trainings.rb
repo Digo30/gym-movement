093d1867f6ccfe7ad52bb442ea73d7fb04af0e76
Gilmar Shuenck <gilmar_shuenck@hotmail.com>
2024-12-12 17:59:56 -0300

    fix bug

--- db/migrate/20241211200443_create_trainings.rb
+++ db/migrate/20241211200443_create_trainings.rb
@@ -2,7 +2,7 @@ class CreateTrainings < ActiveRecord::Migration[6.0]
   def change
     unless table_exists?(:trainings)
       create_table :trainings do |t|
-        t.string :name
+        t.string :title
         t.text :description
         t.integer :duration
         t.timestamps
