07396866495c0cb4b9cd115d9857096fb986f36c
Gilmar Shuenck <gilmar_shuenck@hotmail.com>
2024-12-12 17:55:39 -0300

    fix bug

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,8 +1,10 @@
 function scrollToBottom() {
   var messagesContainer = document.getElementById("messages-container2");
   const chatInput = document.querySelector(".chat-input");
-  messagesContainer.scrollTop = messagesContainer.scrollHeight;
-  window.scrollTo({ top: chatInput.getBoundingClientRect().top + window.scrollY + 100 });
+  if (messagesContainer && chatInput){
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    window.scrollTo({ top: chatInput.getBoundingClientRect().top + window.scrollY + 100 });
+  }
 }
 
 window.addEventListener("load", function () {
--- db/migrate/20241212192808_add_user_id_to_trainings.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToTrainings < ActiveRecord::Migration[7.2]
-  def change
-    add_column :trainings, :user_id, :bigint
-    add_index :trainings, :user_id
-  end
-end
