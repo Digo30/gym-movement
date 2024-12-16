829784fab41356448cdfb845ecbd4e091acf7703
alvaro bernucci <alvarosenabernucci@hotmail.com>
2024-12-16 11:28:28 -0300

    Ajustes Responsivos

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -1,14 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["alert"]
+  static targets = ["alert"];
 
   connect() {
     // Initialize controller
   }
 
   showAlert() {
-    const alertDiv = document.createElement("div")
+    const alertDiv = document.createElement("div");
     alertDiv.innerHTML = `
       <div class="custom-alert">
         <div class="alert-content">
@@ -18,26 +18,26 @@ export default class extends Controller {
           <p>Aguarde, seu treino está sendo criado...</p>
         </div>
       </div>
-    `
-    document.body.appendChild(alertDiv)
+    `;
+    document.body.appendChild(alertDiv);
 
     // Remove alert after 7 seconds
     setTimeout(() => {
-      alertDiv.remove()
-      this.showViewTrainingButton()
-    }, 7000)
+      alertDiv.remove();
+      this.showViewTrainingButton();
+    }, 7000);
   }
 
   showViewTrainingButton() {
-    const buttonDiv = document.createElement("div")
+    const buttonDiv = document.createElement("div");
     buttonDiv.innerHTML = `
       <div class="text-center mt-4">
-        <a href="${this.element.dataset.userTrainingsPath}" class="btn btn-primary">
+        <a href="${this.element.dataset.userTrainingsPath}" class="btn btn-success rounded-custom">
           Ver meu treino
         </a>
       </div>
-    `
-    const formCard = this.element.querySelector('.form-card')
-    formCard.appendChild(buttonDiv)
+    `;
+    const formCard = this.element.querySelector(".form-card");
+    formCard.appendChild(buttonDiv);
   }
 }
